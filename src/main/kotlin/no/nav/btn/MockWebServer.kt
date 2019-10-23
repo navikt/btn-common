188f3701c2282d27e4b31ef9fe808a0644581ad4
Richard Borge <richard.borge@nav.no>
2019-10-23 09:18:00 +0200

    OPP-968 Lager et mock server kall for å simulere feiling og delay

---
+++ src/main/kotlin/no/nav/btn/MockWebServer.kt
@@ -0,0 +1,10 @@
+package no.nav.btn
+
+import java.lang.Exception
+
+fun makeMockServerCall() {
+    Thread.sleep(100)
+    if (Math.random() * 10 == 5.0) {
+        throw Exception("Fail")
+    }
+}
