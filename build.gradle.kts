358ec9963d01f545768ac05b37b5f81bbc7aa78e
Richard Borge <richard.borge@nav.no>
2019-10-23 12:05:37 +0200

    OPP-968 Release pipeline

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.50"
     `java-library`
+    `maven-publish`
 }
 
 repositories {
@@ -13,4 +14,39 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+publishing {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/navikt/btn-common")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_PASSWORD")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+            pom {
+                name.set("btn-common")
+                description.set("Bibliotek for tidslinjer av intervaller relatert til sykefravær")
+                url.set("https://github.com/navikt/btn-common")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:https://github.com/navikt/btn-common.git")
+                    developerConnection.set("scm:git:https://github.com/navikt/btn-common.git")
+                    url.set("https://github.com/navikt/btn-common")
+                }
+            }
+            from(components["java"])
+        }
+    }
 }
