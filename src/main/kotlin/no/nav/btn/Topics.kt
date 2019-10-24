01d975fdcae0f8a3514df52b2387764834d28da3
Richard Borge <richard.borge@nav.no>
2019-10-24 09:20:42 +0200

    OPP-968 Nye topics for retry

--- src/main/kotlin/no/nav/btn/Topics.kt
+++ src/main/kotlin/no/nav/btn/Topics.kt
@@ -2,3 +2,5 @@ package no.nav.btn
 
 const val TOPIC_MELDING_FRA_BRUKER = "btn-brukermelding"
 const val TOPIC_MELDING_MED_OPPGAVE = "btn-brukermelding-oppgave"
+const val TOPIC_RETRY_OPPGAVE_OPPRETTELSE = "btn-opprett-oppgave-retry"
+const val TOPIC_MULTIPLE_FAILING_SERVICE_CALLS = "btn-multiple-failing"
